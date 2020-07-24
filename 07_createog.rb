3b3b8929bc1d00042da67f860b2ec6b0cad287d2
HtYz <daisuke.yajima1380@gmail.com>
2020-07-25 01:47:41 +0900

    Update 07_createog.rb
    changed to create multiple organization group based on utf8 encoded csv files

--- 07_createog.rb
+++ 07_createog.rb
@@ -1,20 +1,24 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require "csv"
 
-##########################################
+#
 # Set variables
-##########################################
+#
 
 cn = ''
 $accountname = ''
-$passwd = 'hoge'
+$passwd = ''
 $rest_key = ''
-parent_og = ''
+parent_og = '' # numeric number 
 
-##########################################
-# Define a method to perform Delete or Get 
-##########################################
+$ary = CSV.read("") # testdata csv filename
+$params = $ary.shift
+
+#
+# Method to create a post request
+#
 
 def uem_req(name, api_uri, bodydata)
 
@@ -22,26 +26,39 @@ def uem_req(name, api_uri, bodydata)
 
   name = Net::HTTP::Post.new(uri.request_uri)
 
-  name.basic_auth($accountname, $passwd ) 
+  name.basic_auth($accountname, $passwd )
   name.add_field('aw-tenant-code', $rest_key )
   name.add_field('Accept','application/json' )
 
   name.set_form_data(bodydata)
- 
+
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
-  http.start {|i| i.request(name) }
+  http.start {|h| h.request(name) }
 
 end
 
-##########################################
+#
 # Perform a Get and parse the response to get a specified organization group
-##########################################
+#
 
 api = 'https://as' + cn + '.awmdm.jp/API/system/groups/' + parent_og
-puts api
-bdt = { "Name": "String", "GroupId": "String",  "LocationGroupType": "Container", "Country": "Japan", "Locale": "ja-jp", "AddDefaultLocation": "true", "Devices": "0", "EnableRestApiAccess": false, "Timezone": 13 }
 
-uem_req('new_og', api, bdt)
+#
 
+$ary.each do |i|
 
+  a = !i[7]
+  b = i[8].to_i
+
+  i.pop(2)
+  i.push(a,b)
+
+  # create a body data to post 
+  row = CSV::Row.new($params,i)
+  puts row.to_hash.to_json
+  bdt = row.to_hash
+
+  uem_req('new_og', api, bdt)
+
+end
