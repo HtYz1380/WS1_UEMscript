f9d867cfb7bb89db8bf7696cca881d502234938e
HtYz <daisuke.yajima1380@gmail.com>
2020-10-11 21:12:54 +0900

    Update 02_get_user_list.rb

--- 02_get_user_list.rb
+++ 02_get_user_list.rb
@@ -1,12 +1,14 @@
 require 'net/http'
 require 'uri'
 
+##set variables 
 cn = ''
 gid = ''
 accountname = ''
 passwd = ''
 rest_key = ''
 
+# creating http request
 uri = URI.parse('https://as' + cn +'.awmdm.com/API/system/users/search?locationgroupId=' + gid)
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
@@ -17,7 +19,9 @@ req.basic_auth(accountname, passwd )
 req.add_field('aw-tenant-code', rest_key )
 req.add_field('Accept','application/json' )
 
+# getting response
 res = http.request(req)
 
+# parsing response 
 puts res.code, res.msg
 puts res.body
