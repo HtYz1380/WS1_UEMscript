b5091f341952eaa07802e235fd48159967ee7385
HtYz <daisuke.yajima1380@gmail.com>
2020-10-11 21:14:31 +0900

    Update 03_og_lists.rb

--- 03_og_lists.rb
+++ 03_og_lists.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+# set variables
 cn = ''
 gid = ''
 accountname = ''
@@ -9,6 +10,7 @@ passwd = ''
 rest_key = ''
 srch = ''
 
+# creating http request
 uri = URI.parse('https://as' + cn +'.awmdm.com/API/system/groups/' + gid + '/children')
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
@@ -19,9 +21,12 @@ req.basic_auth(accountname, passwd )
 req.add_field('aw-tenant-code', rest_key )
 req.add_field('Accept','application/json' )
 
+# getting response
 res = http.request(req)
 
 puts res.code, res.msg
+
+# parsing response
 ary = JSON.parse(res.body)
 hash = Hash.new()
 
@@ -31,4 +36,5 @@ ary.each do |i|
   end
 end
 
+# print the parsed response
 puts hash
