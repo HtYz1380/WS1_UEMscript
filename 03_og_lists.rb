49a5b78b583dd9ca5966d254fd0711d42e857146
htyz1380 <yajimatest1358@gmail.com>
2025-03-07 00:17:10 +0900

    same style

--- 03_og_lists.rb
+++ 03_og_lists.rb
@@ -2,33 +2,53 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+#
+# Set variables
+#
 
-# set variables
+# REST API endpoint FQDN
+apihost = ''
 
-cn = ''
+# Group ID on the Admin Console
 gid = ''
-accountname = ''
-passwd = ''
-rest_key = ''
+
+# Account name that has access right to the API Service
+$accountname = ''
+
+# Password of obove Admin Account
+$passwd = ''
+
+# Rest API Key found on the Admin Console
+$rest_key = ''
+
 srch = ''
 
+#
 # creating http request
-uri = URI.parse('https://as' + cn +'.awmdm.com/API/system/groups/' + gid + '/children')
+#
+
+uri = URI.parse('https://' + apihost + '/API/system/groups/' + gid + '/children')
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 
 req = Net::HTTP::Get.new(uri.request_uri)
 
-req.basic_auth(accountname, passwd ) 
-req.add_field('aw-tenant-code', rest_key )
+req.basic_auth($accountname, passwd ) 
+req.add_field('aw-tenant-code',$rest_key )
 req.add_field('Accept','application/json' )
 
+#
 # getting response
+#
+
 res = http.request(req)
 
 puts res.code, res.msg
 
+#
 # parsing response
+#
+
 ary = JSON.parse(res.body)
 hash = Hash.new()
 
@@ -38,5 +58,8 @@ ary.each do |i|
   end
 end
 
+#
 # print the parsed response
+#
+
 puts hash
